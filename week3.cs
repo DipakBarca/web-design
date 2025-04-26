0b4fa96db03024a37ebf1774f45c5d13bf3b388a
DipakBarca <mahato.d8877@gmail.com>
2025-04-26 22:31:51 +1000

    Add files via upload

---
+++ week3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person("Alex", 20);
+
+        Console.WriteLine("Name: " + person1.Name);
+        Console.WriteLine("Name: " + person1.Age);
+    }
+}
